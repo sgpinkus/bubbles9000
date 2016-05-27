523711db29661b4ac8e58975b6aae4e160517026
sam-at-github <sgpinkus@gmail.com>
2016-05-27 13:09:28 +1000

    Update evolver harness script and java code.

--- script/evolve.sh
+++ script/evolve.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Harness for automatically evolving bots.
 # Currently game reports results to JSON files in /tmp
-cd `dirname $0`
+cd "`dirname $0`/.."
 RESULTSFILE="/tmp/result-pool.json"
 CONFIGSFILE="/tmp/config-pool.json"
 RESULTSDIR="results-`date +%F-%H%M%S`"
-TOTALGENS=1000
-RUNSPERGEN=8
+TOTALGENS=100
+RUNSPERGEN=20
 PAR=3
 
 [[ -d $RESULTSDIR ]] || mkdir $RESULTSDIR
@@ -16,7 +16,7 @@ rm $RESULTSFILE $CONFIGSFILE 2>/dev/null
 # Evo loop. Note there is currently 3 bots per run. Means the pool size will be 30.
 for i in `seq 1 $TOTALGENS`; do
   for j in `seq 1 $RUNSPERGEN`; do
-     echo "############ Playing game $((($i-1)*$RUNSPERGEN+$j)) ############"
+    echo "############ Playing game $((($i-1)*$RUNSPERGEN+$j)) ############"
     ./Bubbles9000/application.linux64/Bubbles9000 #>/dev/null 2>&1
   done
   echo "############ Evolution $i ############"
